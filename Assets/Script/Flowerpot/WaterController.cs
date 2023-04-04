6de8ffb3aa216fdd04197b9ffd50eabbb97a6c43
コトレ <cotore-game@users.noreply.github.com>
2023-04-04 20:30:20 +0900

    しずくの基本処理全般
    ・落ちる処理
    ・落ちきったら消える
    ・触れたら消える

--- Assets/Script/Flowerpot/WaterController.cs
+++ Assets/Script/Flowerpot/WaterController.cs
@@ -4,59 +4,33 @@ using UnityEngine;
 
 public class WaterController : MonoBehaviour
 {
-    //GameObject
-    public GameObject WaterDropInstance;
-    Vector2 position;
+    private float FallSpeed = -3f;
+    private WaterGeneraeter w;
 
-    //移動制限
-    const float UPPER_GENERATE_POS = 6f;
-    const float LEFT_GENERATE_POS = -8f;
-    const float RIGHT_GENERATE_POS = 2.3f;
+    const float DeletePos = -6f;
 
-    public int FallSpeed = 8;
-
-    //生成タイマー
-    private float Timer = 0f;
-
-    //フェーズ
-    [SerializeField] private int PresentFase = 1;
-
-    void Update()
+    private void Start()
     {
-        Timer += Time.deltaTime;
-        position = WaterDropInstance.transform.position;
-
-        //ゲーム難易度
-        switch (PresentFase)
-        {
-            case 1:
-                //Debug.Log("Fase1");
-                FallSpeed = 8;
-                break;
-
-            case 2:
-                //Debug.Log("Fase2");
-                FallSpeed = 10;
-                break;
-
-            case 3:
-                //Debug.Log("Fase3");
-                FallSpeed = 15;
-                break;
-        }
-
-        if(Timer > 10f)
-        {
-            InstantiateWaterDrop(LEFT_GENERATE_POS, RIGHT_GENERATE_POS, UPPER_GENERATE_POS);
-            Timer -= 10f;
-        }
-
-        position.y -= FallSpeed * Time.deltaTime;
+        /*
+        w = GetComponent<WaterGeneraeter>();
+        FallSpeed = w.FallSpeed;
+        Debug.Log(FallSpeed);
+        */
     }
 
-    public void InstantiateWaterDrop(float left, float right, float upper)
+    private void Update()
     {
-        //インスタンスを生成する
-        Instantiate(WaterDropInstance, new Vector2(Random.Range(left, right), upper), Quaternion.identity);
+        transform.Translate(0f, FallSpeed * Time.deltaTime, 0f);
+
+        if (transform.position.y < DeletePos)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("当たり判定");
+        Destroy(gameObject);
     }
 }
--- Assets/Script/Flowerpot/WaterGeneraeter.cs
+++ Assets/Script/Flowerpot/WaterGeneraeter.cs
@@ -4,15 +4,63 @@ using UnityEngine;
 
 public class WaterGeneraeter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //GameObject
+    public GameObject WaterDropInstance;
 
-    // Update is called once per frame
+    //移動制限
+    const float UPPER_GENERATE_POS = 6f;
+    const float LEFT_GENERATE_POS = -8f;
+    const float RIGHT_GENERATE_POS = 2.3f;
+
+    public int FallSpeed = -8;
+
+    //生成タイマー
+    private float Timer = 0f;
+    public float IntervalTimer = 5f;
+
+    //フェーズ
+    [SerializeField] private int PresentFase = 1;
+
     void Update()
     {
-        
+        Timer += Time.deltaTime;
+
+        /*
+        //ゲーム難易度
+        switch (PresentFase)
+        {
+            case 1:
+                //Debug.Log("Fase1");
+                FallSpeed = -8;
+                IntervalTimer = 5f;
+                break;
+
+            case 2:
+                //Debug.Log("Fase2");
+                FallSpeed = -10;
+                IntervalTimer = 3f;
+                break;
+
+            case 3:
+                //Debug.Log("Fase3");
+                FallSpeed = -15;
+                IntervalTimer = 0.8f;
+                break;
+        }
+        */
+
+        if (Timer > IntervalTimer)
+        {
+            InstantiateWaterDrop(LEFT_GENERATE_POS, RIGHT_GENERATE_POS, UPPER_GENERATE_POS);
+            Timer -= IntervalTimer;
+        }
+    }
+
+    public void InstantiateWaterDrop(float left, float right, float upper)
+    {
+        //インスタンスを生成する
+        Instantiate(WaterDropInstance, new Vector2(Random.Range(left, right), upper), Quaternion.identity);
+
+        Debug.Log("しずくの生成");
     }
 }
